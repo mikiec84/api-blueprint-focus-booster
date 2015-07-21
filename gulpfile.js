9c4cb37af5375fbe72e03e35cd9e070c60b7a262
Samir Djellil <samir@djellil.com>
2015-07-21 11:14:12 +0200

    Refactoring.

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,43 @@
 'use strict';
 
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var config = require('./config.js');
-var extractJsonLike = require('gulp-extract-json-like');
-var jsonLint = require('gulp-json-lint');
-var replace = require('gulp-replace');
-var shell = require('gulp-shell');
-var soften = require('gulp-soften');
-var template = require('gulp-template');
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  config = require('./config.js'),
+  extractJsonLike = require('gulp-extract-json-like'),
+  jsonLint = require('gulp-json-lint'),
+  replace = require('gulp-replace'),
+  shell = require('gulp-shell'),
+  soften = require('gulp-soften'),
+  template = require('gulp-template');
 
-var srcDir = './src/**/*.apib';
-var outDir = './build/';
-var outFile = 'output.apib';
+var srcDir = './src/**/*.apib',
+  outDir = './build/',
+  outFile = 'output.apib';
+
+gulp.task('default', ['build']);
+
+gulp.task('build', ['validate', 'main']);
 
 gulp.task('validate', function() {
-	gulp.src(srcDir)
-		.pipe(template(config))
-		.pipe(replace(/^( {8}[^{]\S.*|\S.*| {4}(\+|-|\*).*)/gm, '')) // try to remove useless text
-		.pipe(extractJsonLike())
-		.pipe(jsonLint())
-		.pipe(jsonLint.report('verbose'));
+  gulp.src(srcDir)
+    .pipe(template(config))
+    // try to remove useless text
+    .pipe(replace(/^( {8}[^{]\S.*|\S.*| {4}(\+|-|\*).*)/gm, ''))
+    .pipe(extractJsonLike())
+    .pipe(jsonLint())
+    .pipe(jsonLint.report('verbose'));
 });
 
-gulp.task('main', ['validate'], function() {
-	gulp.src(srcDir)
-		.pipe(template(config))
-		.pipe(soften(4))
-		.pipe(concat('output.apib'))
-		.pipe(gulp.dest('./build/'));
+gulp.task('main', function() {
+  gulp.src(srcDir)
+    .pipe(template(config))
+    .pipe(soften(4))
+    .pipe(concat('output.apib'))
+    .pipe(gulp.dest('./build/'));
 });
 
 gulp.task('publish', shell.task([
-	'export APIARY_API_KEY=' + config.apiary.token + ' && \
+  'export APIARY_API_KEY=' + config.apiary.token + ' && \
 		apiary publish --api-name=' + config.apiary.name + ' \
 		--path=' + outDir + outFile
 ]));
-
-gulp.task('build', ['main']);
-
-gulp.task('default', ['build']);
