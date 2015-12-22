f731b51f1bae622a7acfc48c7e1297be04dd08a2
Michal Dydecki <michaldydecki@gmail.com>
2015-12-22 14:17:02 +0100

    added gulp serve task with livereload

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,9 @@ var gulp = require('gulp'),
   replace = require('gulp-replace'),
   shell = require('gulp-shell'),
   soften = require('gulp-soften'),
+  aglio = require('gulp-aglio'),
+  runSequence = require('run-sequence'),
+  server = require('gulp-server-livereload'),
   template = require('gulp-template');
 
 var srcDir = './src/**/*.apib',
@@ -18,8 +21,8 @@ gulp.task('default', ['build']);
 
 gulp.task('build', ['validate', 'main']);
 
-gulp.task('validate', function() {
-  gulp.src(srcDir)
+gulp.task('validate', function () {
+  return gulp.src(srcDir)
     .pipe(template(config))
     // try to remove useless text
     .pipe(replace(/^( {8}[^{]\S.*|\S.*| {4}(\+|-|\*).*)/gm, ''))
@@ -28,12 +31,33 @@ gulp.task('validate', function() {
     .pipe(jsonLint.report('verbose'));
 });
 
-gulp.task('main', function() {
-  gulp.src(srcDir)
+gulp.task('main', function () {
+  return gulp.src(srcDir)
     .pipe(template(config))
     .pipe(soften(4))
     .pipe(concat('output.apib'))
     .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('serve', function () {
+  runSequence('validate', 'main', 'aglio', function() {
+    gulp.src(outDir)
+      .pipe(server({
+        livereload: true,
+        port: 3005,
+        defaultFile: 'output.html'
+      }));
+
+    gulp.watch(srcDir, function () {
+      runSequence('validate', 'main', 'aglio');
+    });
+  });
+});
+
+gulp.task('aglio', function () {
+  return gulp.src(outDir + 'output.apib')
+    .pipe(aglio())
+    .pipe(gulp.dest('build'));
 });
 
 gulp.task('publish', shell.task([
